ba537adc2006acedebf0234006226ede0d14fd80
lulamae12 <tls.tommy11@gmail.com>
2019-10-22 22:49:18 -0500

    made chg

--- destinyWeb/destinyApp/views.py
+++ destinyWeb/destinyApp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json,os
+
+from pprint import pprint
 # Create your views here.
 
 def index(request):
-    
-    
-    
+    jFile = "/Destiny1Grimoire/allies/rasputin"
+
+    cwd = os.getcwd()
+    jsonData = []
+    with open(str(cwd+jFile + "/grimoireJson.json")) as jsonFile:
+        data = json.load(jsonFile)
+       
+    pprint(data["name"])
+            #test = input()
     return HttpResponse("you are at the destiny app index")
 
 def cardHome(request):
